85d1149048a5303a568349fc03bd5ded13d2d9aa
jacekdmochowski <dmochowski@gmail.com>
2023-09-04 21:30:00 -0400

    add answer log

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+from flask import Flask, render_template, request, session
 import random
 import pygame
+import numpy as np
+import datetime
+
 
 app = Flask(__name__)
+app.secret_key = '3432'
 
 # Initialize pygame
 pygame.mixer.init()
@@ -16,9 +21,8 @@ incorrect_sounds = [pygame.mixer.Sound(x) for x in incorrect_mp3]
 
 correct_answers = 0
 total_questions_attempted = 0
-
-#correct_sound = pygame.mixer.Sound("Coolesteva.mp3")
-#incorrect_sound = pygame.mixer.Sound("suprsuck.mp3")
+answer_log = {'num1':[], 'num2':[], 'answer':[], 'is_correct':[]}
+datestr = datetime.datetime.now().strftime('%m%d%y-%H%M%S')
 
 # Define the range of multiplication questions
 min_number = 1
@@ -30,6 +34,12 @@ def index():
 
 @app.route('/ask_question', methods=['POST'])
 def ask_question():
+    name = request.form['name']
+    age = request.form['age']
+
+    session['name'] = name
+    session['age'] = age
+
     # Generate random numbers for the question
     num1 = random.randint(min_number, max_number)
     num2 = random.randint(min_number, max_number)
@@ -37,61 +47,52 @@ def ask_question():
     # Calculate the correct answer
     correct_answer = num1 * num2
 
-    return render_template('question.html', num1=num1, num2=num2, correct_answer=correct_answer)
+    return render_template('question.html', num1=num1, num2=num2, correct_answer=correct_answer, name=name, age=age)
 
-""" 
-@app.route('/check_answer', methods=['POST'])
-def check_answer():
-    #num1 = int(request.form['num1'])
-    #num2 = int(request.form['num2'])
-    user_answer = int(request.form['user_answer'])
-    correct_answer = int(request.form['correct_answer'])
 
-    num1 = int(request.form['num1'])
-    num2 = int(request.form['num2'])
-
-    global correct_answers
-    global total_questions_attempted
-
-    total_questions_attempted += 1
-
-    if user_answer == correct_answer:
-        correct_answers += 1
-        ii=random.randint(0,len(correct_sounds)-1)
-        pygame.mixer.Sound.play(correct_sounds[ii])
-        #pygame.mixer.Sound.play(correct_sound)
-        return render_template('correct.html', num1=num1, num2=num2, correct_answer=correct_answer)
-    else:
-        ii=random.randint(0,len(incorrect_sounds)-1)
-        pygame.mixer.Sound.play(incorrect_sounds[ii])
-        #pygame.mixer.Sound.play(incorrect_sound)
-        return render_template('incorrect.html', num1=num1, num2=num2, correct_answer=correct_answer)
-"""
 
 @app.route('/check_answer', methods=['POST'])
 def check_answer():
+
+    #name = session.get('name', 'Guest')
+    #age = session.get('age', 'N/A')
+
+    name = request.form['name']
+    age = request.form['age']
+
     user_answer = int(request.form['user_answer'])
     correct_answer = int(request.form['correct_answer'])
 
     global correct_answers
     global total_questions_attempted
+    global answer_log
+    global datestr
 
     total_questions_attempted += 1
 
     num1 = int(request.form['num1'])
     num2 = int(request.form['num2'])
 
+    answer_log['num1'].append(str(num1))
+    answer_log['num2'].append(str(num2))
+    answer_log['answer'].append(str(user_answer))
+    answer_log['is_correct'].append(str(user_answer==correct_answer))
+    print(answer_log)
+
+    csv_filename=datestr+'_'+name+'_'+age+'_test_answer_log'
+    pd.DataFrame(answer_log,index=np.array(range(total_questions_attempted))).to_csv(csv_filename)
+
     if user_answer == correct_answer:
         correct_answers += 1
         ii=random.randint(0,len(correct_sounds)-1)
         pygame.mixer.Sound.play(correct_sounds[ii])
-        #pygame.mixer.Sound.play(correct_sound)
         return render_template('correct.html', num1=num1, num2=num2, correct_answer=correct_answer)
     else:
         ii=random.randint(0,len(incorrect_sounds)-1)
         pygame.mixer.Sound.play(incorrect_sounds[ii])
-        #pygame.mixer.Sound.play(incorrect_sound)
         return render_template('incorrect.html', num1=num1, num2=num2, correct_answer=correct_answer)
+
+
 
 
 
